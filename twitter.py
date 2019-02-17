5ad68523c279d49c5f7c55d3263df11fa9a7d070
anarghya <anarghya@buffalo.edu>
2019-02-17 09:38:23 -0500

    :sparkles: updated to requests and wegmans api works now

--- twitter.py
+++ twitter.py
@@ -1,18 +1,20 @@
+import requests
 import json
 import random 
 import bot
-from flask import current_app
-
 
 def tweetTweet():
-    with current_app.open_resource("{{url_for('static',filename='pic.png')}}",'r') as f:
-        js=f.read()
+    headers = {
+        # Request headers
+        'Subscription-Key': '3a5f419cd2b04164933ee603e990b8ff',
+    }
 
-    ob=json.loads(js)
-    arr=ob['recipes']
+    r = requests.get('https://api.wegmans.io/meals/recipes?api-version=2018-10-18', params=headers)
+
+    obj=json.loads(r.content)
+    arr=obj['recipes']
     finalArr=[]
     for o in arr:
-        finalArr.append(o['name'])
-
+            finalArr.append(o['name'])
     idx=random.randint(0,1914)
     bot.tweet(finalArr[idx])
