22f152609672e9174349ee93c04c1442004ece3f
anarghya <anarghya@buffalo.edu>
2019-02-17 06:13:01 -0500

    :sparkles: fouond a way to print json prettyly

--- tester.py
+++
@@ -1,9 +0,0 @@
-import json
-import urllib.request
-from pyowm import OWM
-API_KEY='106740011b0ccfb24c15328a94f6b7c9'
-owm=OWM(API_KEY)
-c=owm.weather_at_place("New York, New York")
-c1=c.get_weather()
-c2=c1.to_JSON
-print(c2)
