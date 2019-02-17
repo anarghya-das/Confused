ce31285cde6b3a09836c6bcc9a6e9066f2ffa3f9
Moulid Ahmed <moulidah@buffalo.edu>
2019-02-17 02:36:44 -0500

    country data

--- counter.py
+++ counter.py
@@ -2,7 +2,11 @@ import json
 import urllib.request
 from pyowm import OWM
 
+
 class WeatherText(object):
+    def __init__(self):
+        self.mapper = {}
+
     def toarr(self):
         apiKey = "NMX2QYEuqSjNe8MBf6gE7uHoTSeu7KIB"
         results = 50  # 50,100,150
@@ -17,8 +21,24 @@ class WeatherText(object):
     def get_key(self):
         weather_type = []
         nodups = []
+        ofmap = {}
+        lon = {}
+        lat = {}
+        l = []
         arr = self.toarr()
         for i in range(len(arr)):
+            lon.clear()
+            lat.clear()
+            l.clear()
+            ofmap.clear()
+            lon['Longitude'] = arr[i]['GeoPosition']['Longitude']
+            lat['Latitude'] = arr[i]['GeoPosition']['Latitude']
+            ofmap['Country'] = arr[i]['EnglishName']
+            l.append(ofmap)
+            l.append(lon)
+            l.append(lat)
+            print(l)
+            self.mapper[arr[i]['WeatherText']] = l
             weather_type.append(arr[i]['WeatherText'])
         for i in weather_type:
             if i in nodups:
@@ -27,5 +47,10 @@ class WeatherText(object):
                 nodups.append(i)
         return json.dumps(nodups)
 
+    def getMapper(self):
+        return self.mapper
 
 
+obj = WeatherText()
+print(obj.getMapper())
+obj.get_key()
